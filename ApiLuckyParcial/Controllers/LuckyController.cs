b0abe874351c13b28544107282c1214973efdef6
Adriana Yabeta Rappu <a2020111837@estudiantes.upsa.edu.bo>
2021-10-13 19:35:00 -0400

    Agregar archivos de proyecto.

---
+++ ApiLuckyParcial/Controllers/LuckyController.cs
@@ -0,0 +1,45 @@
+﻿using ApiLuckyParcial.Data;
+using ApiLuckyParcial.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiLuckyParcial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LuckyController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public LuckyController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+		[HttpGet]
+		public async Task<ActionResult<Suerte>> GetSuerte()
+		{
+			var list = await _context.Suerte.ToListAsync();
+
+			var max = list.Count;
+			int index = new Random().Next(0, max);
+
+			var suerte = list[index];
+
+			if (suerte == null)
+			{
+				return NoContent();
+			}
+
+			return suerte;
+
+
+		}
+
+	}
+}
---
+++ ApiLuckyParcial/Models/Suerte.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiLuckyParcial.Models
+{
+    public class Suerte
+    {
+        [Key]
+        public int SuerteId { get; set; }
+        [Required(ErrorMessage = "Suerte de la persona")]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Debe tener entre 5 a 80 caracteres")]
+        [Display(Name = "Suerte")]
+        public string Detalle { get; set; }
+        [Url]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "La longitd de la imagen debe tener entre 2 a 100 caracteres")]
+        [Display(Name = "Imagen")]
+        public string Imagen { get; set; }
+
+    }
+}
